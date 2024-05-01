114ceb0e9ceaf9ad760e88cc2c5f2992a7ea5da6
Yaroslav-Bonadarenko <consultant@meanrev.com>
2024-05-02 01:42:12 +0300

    added

---
+++ src/app/dashboard/services/components/google-autocomplete.tsx
@@ -0,0 +1,40 @@
+import React, { useRef, useCallback } from 'react';
+import { useLoadScript, Autocomplete } from '@react-google-maps/api';
+import {CommandInput} from "@/components/ui/command";
+
+const libraries = ["places"];
+
+const AutoCompleteInput = ({ placeholder, onPlaceSelected }) => {
+    const ref = useRef(null);
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: 'AIzaSyAul_Yd7Vqd35uBuWMrUz-f1cF52b2WSJ8',
+        libraries,
+    });
+
+    const handleLoad = useCallback((autocomplete) => {
+        console.log("Autocomplete is loaded");
+    }, []);
+
+    const handleChange = useCallback(() => {
+        console.log('place', ref.current);
+        if (ref.current && onPlaceSelected) {
+            const place = ref.current.getPlace();
+            onPlaceSelected(place);
+        }
+    }, [onPlaceSelected]);
+
+    return isLoaded ? (
+        <Autocomplete
+            onLoad={handleLoad}
+            onPlaceChanged={handleChange}
+        >
+            <CommandInput
+                ref={ref}
+                placeholder={placeholder}
+            />
+        </Autocomplete>
+    ) : null;
+};
+
+export default AutoCompleteInput;
