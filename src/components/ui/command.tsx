cdb55451791390fe10b486f508530fe48c74cc5b
Yaroslav-Bonadarenko <consultant@meanrev.com>
2024-05-02 09:35:47 +0300

    added

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -40,7 +40,7 @@ const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
 const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
->(({ className, ref, ...props }) => (
+>(({ className, ...props }, ref) => (
   <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
     <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
     <CommandPrimitive.Input
