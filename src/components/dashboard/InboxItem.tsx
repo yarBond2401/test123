6c9495c9a583b891e60560e29a50c8ad4363a891
yarBond2401 <yariks2401@gmail.com>
2024-05-05 22:48:36 +0300

    fix adoption and charts

--- src/components/dashboard/InboxItem.tsx
+++ src/components/dashboard/InboxItem.tsx
@@ -13,21 +13,21 @@ interface Props {
 export const InboxItem: FC<Props> = ({ item }) => {
   return (
     <Link href="/dashboard/chat">
-      <div className="flex flex-row gap-3.5 px-[15px] py-[10px] items-center hover:bg-[#F9FAFB] rounded-[5px]">
+      <div className="flex flex-row gap-3.5 2xl:px-[15px] px-1 py-[10px] items-center hover:bg-[#F9FAFB] rounded-[5px]">
         {/* <Avatar.Root className="rounded-full h-11 w-11">
           <Avatar.Image src={item.photo} height={44} width={44} alt={item.name} />
         </Avatar.Root> */}
-        <div className="relative">
-          <Image src={item.photo} height={44} width={44} alt={item.name}/>
+        <div className="relative 2xl:h-11 2xl:w-11 md:h-8 md:w-8 h-11 w-11">
+          <Image src={item.photo} alt={item.name} className="w-full h-auto"/>
           <div className={cn(
             item.online ? "bg-green-400" : "bg-red-500", 
-            "w-3 h-3 rounded-full border-[3px] border-white absolute bottom-0 right-0 box-border"
+            "2xl:w-3 2xl:h-3 md:w-2 md:h-2 w-3 h-3 rounded-full 2xl:border-[2px] md:border border-[2px] border-white absolute bottom-0 right-0 box-border"
           )}/>
         </div>
 
         <div className="flex flex-col">
-          <p className="text-base leading-[22px] font-medium text-dashboard-main">{item.name}</p>
-          <p className="text-sm font-medium text-dashboard-secondary">{item.massage}</p>
+          <p className="2xl:text-base md:text-sm text-base leading-[22px] font-medium text-dashboard-main">{item.name}</p>
+          <p className="2xl:text-sm md:text-xs text-sm font-medium text-dashboard-secondary">{item.massage}</p>
         </div>
       </div>
     </Link>
--- src/components/dashboard/Statistics.tsx
+++ src/components/dashboard/Statistics.tsx
@@ -12,21 +12,21 @@ interface Props {
 
 export const Statistics: FC<Props> = ({ icon, result, total, grow }) => {
   return (
-    <div className="flex bg-white border border-[#DFE4EA] rounded-10 flex-col w-full p-22 gap-5">
+    <div className="flex bg-white border border-[#DFE4EA] rounded-10 flex-col w-full xl:p-22 p-4 gap-5">
       <div className="flex items-center justify-center rounded-full bg-[#5296BF] w-[50px] h-[50px]">
         <Image src={icon} alt="dollar" height={24} width={24} />
       </div>
-      <div className="flex flex-row justify-between w-full">
-        <div className="flex flex-col gap-2">
-          <p className="text-dashboard-main text-2xl leading-[29px] font-bold">
+      <div className="flex xl:flex-row lg:flex-col flex-row gap-2 justify-between w-full">
+        <div className="flex flex-col gap-2 justify-between h-full">
+          <p className="text-dashboard-main xl:text-2xl lg:text-lg text-2xl leading-[29px] font-bold">
             {result}
           </p>
-          <p className="text-dashboard-secondary text-base leading-[22px] font-medium">
+          <p className="text-dashboard-secondary leading-[22px] font-medium 2xl:text-base lg:text-sm text-base">
             Total {total}
           </p>
         </div>
-        <div className="flex flex-row gap-1 items-end">
-          <p className="text-[#089348] text-lg leading-[22px] font-bold">
+        <div className="flex flex-row gap-1 items-end self-end">
+          <p className="text-[#089348] leading-[22px] font-bold 2xl:text-lg lg:text-base text-lg">
             {grow}
           </p>
           <Image src={iconArrow} alt="dollar" height={24} width={24} />
